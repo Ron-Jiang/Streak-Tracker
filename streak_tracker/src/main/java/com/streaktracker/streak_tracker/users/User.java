62cf690ca66e16355abd22b3651be5907a0456fa
Ron-Jiang <ronpbwa11@gmail.com>
2026-03-08 22:06:43 -0700

    Initial User Page, Mongo/Security POM and Config

---
+++ streak_tracker/src/main/java/com/streaktracker/streak_tracker/users/User.java
@@ -0,0 +1,14 @@
+package com.streaktracker.streak_tracker.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+// @NoArgsConstructor
+// @AllArgsConstructor
+public class User {
+
+}
