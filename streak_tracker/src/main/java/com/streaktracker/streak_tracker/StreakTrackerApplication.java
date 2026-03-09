fe73a28c28385800d2be98a220436818dc0409b7
Ron-Jiang <ronpbwa11@gmail.com>
2026-03-08 20:02:36 -0700

    Spring Boot and React Starter Template Setup

---
+++ streak_tracker/src/main/java/com/streaktracker/streak_tracker/StreakTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.streaktracker.streak_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreakTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreakTrackerApplication.class, args);
+	}
+
+}
