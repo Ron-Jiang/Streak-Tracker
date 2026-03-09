25bb3523954e1c13b4e20a02d36a47608d3c66b6
Ron-Jiang <ronpbwa11@gmail.com>
2026-03-09 14:10:10 -0700

    Initial entities and MongoDB repository setup

---
+++ streak_tracker/src/main/java/com/streaktracker/streak_tracker/entities/User.java
@@ -0,0 +1,33 @@
+package com.streaktracker.streak_tracker.entities;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    @Indexed
+    private String email;
+
+    private String userName;
+    private String password;
+    private Instant createdAt;
+
+    @Builder.Default
+    private boolean emailReminders = true;
+
+}
